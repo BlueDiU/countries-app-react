ba6209144da0ecf2d0d9b198b3ade7b5c5e09d28
Josué Rivas <josuerivas2311@gmail.com>
2021-07-09 17:21:08 -0600

    before apply the themes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import AppRouter from './router/AppRouter';
 import { Provider } from 'react-redux';
 import { store } from './context/store';
 import { GlobalStyle } from './App.styles';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
+
       <AppRouter />
     </Provider>
   );
